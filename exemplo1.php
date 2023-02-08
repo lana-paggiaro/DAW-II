146fb9dd5b4ee752c9d383fd977b6c75f9d51924
Lua-da-tarde <luanna@paggiaro.com.br>
2023-02-08 14:45:41 -0300

    agora comecou

---
+++ exemplo1.php
@@ -0,0 +1,16 @@
+<?php
+    echo "Hello World! <br><br>";
+    echo "<b>Olá mundo</b>";
+    echo "<br><br>";
+
+    //------------------------------Variáveis----------------------------
+
+    $nome = "COTIL";
+    echo $nome;
+    echo "<br><br>";
+
+    var_dump($nome); //exibe o tipo de dado, tamanho usado e valor
+    echo "<br><br>";
+
+    
+?>
