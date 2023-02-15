21e637c4412aad3b12ea68a4a710dc141b347a1b
Lua-da-tarde <luanna@paggiaro.com.br>
2023-02-15 11:21:53 -0300

    aula 2

--- exemplo1.php
+++ exemplo1.php
@@ -12,5 +12,53 @@
     var_dump($nome); //exibe o tipo de dado, tamanho usado e valor
     echo "<br><br>";
 
-    
+    $outroNome = "UNICAMP";
+
+    echo $nome . " " . $outroNome;
+    echo "<br><br>";
+
+    unset($nome); //Remove variável. Se quiser limpar várias, basta separar por ","
+
+                 //isset é um função que verifica se a variável existe
+    if(isset($nome)){
+        echo $nome;
+        echo "<br><br>";
+    }else{
+        echo "a variável está vazia <br><br>";
+    }
+
+    $valor = 50.15;
+    echo $valor;
+    echo "<br><br>";
+
+    $aprovado = true;
+    echo $aprovado;
+    echo "<br><br>";
+
+    $disciplinas = array("BD", "LP", "DAW");
+    echo $disciplinas[2];
+    echo "<br><br>";
+
+    $nulo = null;
+    $vazio = "";
+
+    //----------------------------------Constantes--------------------------------------
+
+    define("PI", 3.14);
+    define("NOME_ALUNO", "Maria");
+
+    //https://secure.php.net/manual/pt_BR/reserved.constants.php - constantes pré-definidas do PHP
+
+    $resultado = 3 * PI;
+    echo $resultado . "<BR><BR>";
+    echo "Nome do Aluno: " . NOME_ALUNO . "<BR><BR>";
+
+    //---------------------------------Super Variáveis----------------------------------
+
+    echo "<br><br>";
+    echo $_SERVER["SERVER_ADDR"] . "<BR>";
+    echo $_SERVER["SERVER_NAME"] . "<BR>";
+    echo $_SERVER["HTTP_USER_AGENT"] . "<BR>";
+    echo $_SERVER["REMOTE_ADDR"] . "<BR>";
+    echo $_SERVER["SCRIPT_NAME"] . "<BR>";
 ?>
